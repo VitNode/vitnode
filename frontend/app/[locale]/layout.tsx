9978bb620bb3434fc72178c89396b6235bedb817
aXenDeveloper <axendeveloper@gmail.com>
2023-09-07 19:02:50 +0200

    feat: Add i18n

---
+++ frontend/app/[locale]/layout.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable @next/next/no-sync-scripts */
+import { ReactNode } from 'react';
+import { notFound } from 'next/navigation';
+import { NextIntlClientProvider } from 'next-intl';
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  display: 'swap'
+});
+
+interface Props {
+  children: ReactNode;
+  params: { locale: string };
+}
+
+export default async function LocaleLayout({ children, params: { locale } }: Props) {
+  let messages;
+  try {
+    messages = (await import(`@/messages/${locale}.json`)).default;
+  } catch (error) {
+    notFound();
+  }
+
+  return (
+    <html lang={locale} className={montserrat.className}>
+      <body>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
