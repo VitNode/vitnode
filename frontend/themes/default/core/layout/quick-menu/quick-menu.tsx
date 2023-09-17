fe339d6ff81f020c0e56db29190e4bb6e9d93191
aXenDeveloper <axendeveloper@gmail.com>
2023-09-17 20:04:04 +0200

    chore: Clean up

--- frontend/themes/default/core/layout/quick-menu/quick-menu.tsx
+++ frontend/themes/default/core/layout/quick-menu/quick-menu.tsx
@@ -2,7 +2,7 @@ import { ItemQuickMenu } from './item/item-quick-menu';
 
 export const QuickMenu = () => {
   return (
-    <div className="supports-backdrop-blur:bg-background/60 sticky bottom-0 z-50 w-full border-t bg-card/75 backdrop-blur sm:hidden flex">
+    <div className="supports-backdrop-blur:bg-background/60 fixed bottom-0 z-50 w-full border-t bg-card/75 backdrop-blur sm:hidden flex">
       <ItemQuickMenu active>Test</ItemQuickMenu>
       <ItemQuickMenu>Test</ItemQuickMenu>
       <ItemQuickMenu>Test</ItemQuickMenu>
