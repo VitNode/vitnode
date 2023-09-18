c0df3e65524f021a6ca7a0ca281bf585efcd19d0
aXenDeveloper <axendeveloper@gmail.com>
2023-09-18 16:52:45 +0200

    feat: Add react-query

--- frontend/app/[locale]/theme-provider.tsx
+++ frontend/app/[locale]/theme-provider.tsx
@@ -1,8 +1,20 @@
 'use client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { type ThemeProviderProps } from 'next-themes/dist/types';
+import { useState } from 'react';
 
 export const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <NextThemesProvider {...props}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </NextThemesProvider>
+  );
 };
