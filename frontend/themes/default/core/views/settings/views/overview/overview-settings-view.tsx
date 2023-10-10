e101af6d1c86f8486f8e1436bce5335380c7db62
aXenDeveloper <axendeveloper@gmail.com>
2023-10-10 21:23:15 +0200

    feat: Add support for actions type in notifications toast

--- frontend/themes/default/core/views/settings/views/overview/overview-settings-view.tsx
+++ frontend/themes/default/core/views/settings/views/overview/overview-settings-view.tsx
@@ -10,7 +10,19 @@ export const OverviewSettingsView = () => {
     <Card>
       <HeaderOverviewSettings />
       <CardContent>
-        <button onClick={() => toast.error('Event has not been created')}>Render my toast</button>
+        <button
+          onClick={() =>
+            toast('Event has been created', {
+              action: {
+                label: 'Test 123',
+                // eslint-disable-next-line no-console
+                onClick: () => console.log('Undo')
+              }
+            })
+          }
+        >
+          Render my toast
+        </button>
       </CardContent>
     </Card>
   );
