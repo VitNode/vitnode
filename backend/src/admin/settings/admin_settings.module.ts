3b1734eb7327cee95b76826a133c4ffc22d4d50b
aXenDeveloper <axendeveloper@gmail.com>
2023-10-19 22:59:31 +0200

    feat: Add query for show general settings in AdminCP

--- backend/src/admin/settings/admin_settings.module.ts
+++ backend/src/admin/settings/admin_settings.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 
 import { GeneralAdminSettingsResolver } from './general/general-admin_settings.resolver';
+import { ShowGeneralAdminSettingsService } from './general/show/show-general-admin_settings.service';
 
 @Module({
-  providers: [GeneralAdminSettingsResolver]
+  providers: [GeneralAdminSettingsResolver, ShowGeneralAdminSettingsService]
 })
 export class AdminSettingsModule {}
--- backend/src/admin/settings/general/general-admin_settings.resolver.ts
+++ backend/src/admin/settings/general/general-admin_settings.resolver.ts
@@ -1,6 +1,14 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+
+import { ShowGeneralAdminSettingsService } from './show/show-general-admin_settings.service';
+import { ShowGeneralAdminSettingsObj } from './show/dto/show-general-admin_settings.obj';
 
 @Resolver()
 export class GeneralAdminSettingsResolver {
-  // constructor(private readonly service: AdminAuthorizationCoreSessionsService) {}
+  constructor(private readonly showService: ShowGeneralAdminSettingsService) {}
+
+  @Query(() => ShowGeneralAdminSettingsObj)
+  async show_general_admin_settings(): Promise<ShowGeneralAdminSettingsObj> {
+    return this.showService.show();
+  }
 }
