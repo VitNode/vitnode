0b5cd8ade8bd812530c5c7788834a3b08e13e217
aXenDeveloper <axendeveloper@gmail.com>
2023-12-14 16:40:09 +0100

    fix(editor): Invalid JSON on first load

--- frontend/components/editor/read-only/load.tsx
+++ frontend/components/editor/read-only/load.tsx
@@ -1,8 +1,9 @@
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { useEffect } from 'react';
 import { useLocale } from 'next-intl';
+import { $createParagraphNode, $createTextNode, $getRoot } from 'lexical';
 
-import { TextLanguage } from '../../../graphql/hooks';
+import { TextLanguage } from '@/graphql/hooks';
 
 interface Props {
   value: TextLanguage[];
@@ -12,23 +13,33 @@ export const LoadReadOnlyEditor = ({ value }: Props) => {
   const [editor] = useLexicalComposerContext();
   const locale = useLocale();
 
+  const updateEditorState = (value: string) => {
+    try {
+      const initialEditorState = editor.parseEditorState(value);
+      editor.setEditorState(initialEditorState);
+    } catch (e) {
+      editor.update(() => {
+        const root = $getRoot();
+        root.clear();
+
+        const paragraph = $createParagraphNode();
+        paragraph.append($createTextNode(value));
+        root.append(paragraph);
+      });
+    }
+  };
+
   // Set the initial editor value
   useEffect(() => {
-    if (value.length > 0) {
-      const currentValue = value.find(item => item.id_language === locale)?.value;
+    if (value.length <= 0) return;
 
-      if (!currentValue) {
-        const lastValue = value.at(-1)?.value;
-        if (!lastValue) return;
+    const currentValue = value.find(item => item.id_language === locale)?.value;
+    const lastValue = value.at(-1)?.value;
 
-        const initialEditorState = editor.parseEditorState(lastValue);
-        editor.setEditorState(initialEditorState);
-
-        return;
-      }
-
-      const initialEditorState = editor.parseEditorState(currentValue);
-      editor.setEditorState(initialEditorState);
+    if (!currentValue && lastValue) {
+      updateEditorState(lastValue);
+    } else if (currentValue) {
+      updateEditorState(currentValue);
     }
   }, [locale]);
 
