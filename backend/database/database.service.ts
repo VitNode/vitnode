ac12d2c69cbf6d8deaa824e3ae2a496958635e85
aXenDeveloper <axendeveloper@gmail.com>
2024-01-17 17:38:27 +0100

    feat(docker_compose): Connect redis and database to backend

--- backend/database/database.service.ts
+++ backend/database/database.service.ts
@@ -15,12 +15,20 @@ export class DatabaseService {
   public db: NodePgDatabase<typeof schema>;
 
   constructor() {
+    const envs = {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      name: process.env.DB_NAME
+    };
+
     const pool = new Pool({
-      host: 'localhost',
-      port: 5432,
-      user: 'root',
-      password: 'root',
-      database: 'vitnode'
+      host: envs.host ?? 'localhost',
+      port: envs.port ? +envs.port : 5432,
+      user: envs.user ?? 'root',
+      password: envs.password ?? 'root',
+      database: envs.name ?? 'vitnode'
     });
 
     this.db = drizzle(pool, { schema });
--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,14 +1,22 @@
 import type { Config } from 'drizzle-kit';
 
+const envs = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  NAME: process.env.DB_NAME
+};
+
 export default {
   schema: './src/admin/**/database/schema/*.ts',
   out: './drizzle',
   driver: 'pg',
   dbCredentials: {
-    host: 'localhost',
-    port: 5432,
-    user: 'root',
-    password: 'root',
-    database: 'vitnode'
+    host: envs.host ?? 'localhost',
+    port: envs.port ? +envs.port : 5432,
+    user: envs.user ?? 'root',
+    password: envs.password ?? 'root',
+    database: envs.NAME ?? 'vitnode'
   }
 } satisfies Config;
