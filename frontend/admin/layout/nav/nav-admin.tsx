1647a2d95de5815ae1344f326002e9e3bbb9277c
aXenDeveloper <axendeveloper@gmail.com>
2024-01-21 22:12:04 +0100

    feat(frontend): Add dev mode

--- frontend/admin/layout/nav/nav-admin.tsx
+++ frontend/admin/layout/nav/nav-admin.tsx
@@ -5,7 +5,7 @@ import { LogoVitNode } from '@/components/logo-vitnode';
 export const NavAdmin = () => {
   return (
     <nav className="fixed top-0 left-0 h-screen w-60 overflow-y-auto z-10 hidden sm:block overflow-auto">
-      <div className="sticky top-0 bg-background/75 backdrop-blur flex items-center justify-center h-16 z-50">
+      <div className="sticky top-0 bg-background/75 backdrop-blur flex items-center justify-center h-16 z-40">
         <Link href="/admin/core/dashboard">
           <LogoVitNode className="h-10" />
         </Link>
