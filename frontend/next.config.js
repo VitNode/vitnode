d063f5e2f4f70cf53f1103db9b443b400e4668ad
aXenDeveloper <axendeveloper@gmail.com>
2024-02-13 11:46:57 +0100

    fix(frontend): Add allowedOrigins

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,20 +1,27 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
-const withNextIntl = require('next-intl/plugin')(
+const withNextIntl = require("next-intl/plugin")(
   // This is the default (also the `src` folder is supported out of the box)
-  './i18n.ts'
+  "./i18n.ts"
 );
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true"
 });
 
 const config = () => {
-  const envBackend = process.env.NEXT_PUBLIC_GRAPHQL_URL ?? 'http://localhost:8080';
+  const envBackend =
+    process.env.NEXT_PUBLIC_GRAPHQL_URL ?? "http://localhost:8080";
   const backend = {
     hostname: new URL(envBackend).hostname,
     port: new URL(envBackend).port,
-    protocol: new URL(envBackend).protocol.replace(':', '')
+    protocol: new URL(envBackend).protocol.replace(":", "")
   };
+
+  const allowedOrigins = ["localhost:3000"];
+
+  if (process.env.FRONTEND_URL) {
+    allowedOrigins.push(process.env.FRONTEND_URL);
+  }
 
   return {
     // logging: {
@@ -22,22 +29,27 @@ const config = () => {
     //     fullUrl: true
     //   }
     // },
-    output: 'standalone',
-    transpilePackages: ['lucide-react'],
+    experimental: {
+      serverActions: {
+        allowedOrigins
+      }
+    },
+    output: "standalone",
+    transpilePackages: ["lucide-react"],
     images: {
-      formats: ['image/avif', 'image/webp'],
+      formats: ["image/avif", "image/webp"],
       remotePatterns: [
         {
-          hostname: 'localhost',
-          port: '8080',
-          protocol: 'http',
-          pathname: '/public/**'
+          hostname: "localhost",
+          port: "8080",
+          protocol: "http",
+          pathname: "/public/**"
         },
         {
           hostname: backend.hostname,
           port: backend.port,
           protocol: backend.protocol,
-          pathname: '/public/**'
+          pathname: "/public/**"
         }
       ]
     }
