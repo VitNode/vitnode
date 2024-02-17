d6583dbcce7b3d3eacf091f536856412956f35eb
aXenDeveloper <axendeveloper@gmail.com>
2024-02-17 12:51:41 +0100

    feat(backend): Add delete lang mutation

---
+++ backend/src/admin/core/languages/delete/dto/delete.args.ts
@@ -0,0 +1,7 @@
+import { ArgsType, Field } from "@nestjs/graphql";
+
+@ArgsType()
+export class DeleteCoreAdminLanguagesArgs {
+  @Field(() => String)
+  code: string;
+}
--- backend/src/admin/core/languages/members.module.ts
+++ backend/src/admin/core/languages/members.module.ts
@@ -4,13 +4,17 @@ import { CreateAdminCoreLanguageService } from "./create/create.service";
 import { CreateAdminCoreLanguagesResolver } from "./create/create.resolver";
 import { EditAdminCoreLanguagesResolver } from "./edit/edit.resolver";
 import { EditAdminCoreLanguagesService } from "./edit/edit.service";
+import { DeleteAdminCoreLanguagesResolver } from "./delete/delete.resolver";
+import { DeleteAdminCoreLanguageService } from "./delete/delete.service";
 
 @Module({
   providers: [
     CreateAdminCoreLanguageService,
     CreateAdminCoreLanguagesResolver,
     EditAdminCoreLanguagesResolver,
-    EditAdminCoreLanguagesService
+    EditAdminCoreLanguagesService,
+    DeleteAdminCoreLanguagesResolver,
+    DeleteAdminCoreLanguageService
   ]
 })
 export class AdminLanguagesModule {}
