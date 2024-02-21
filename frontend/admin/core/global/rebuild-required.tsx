d46fa14c9205382a4d45d61ea5bf4c30d6a4ceec
aXenDeveloper <axendeveloper@gmail.com>
2024-02-21 12:24:18 +0100

    docs: Add page how to rebuild app

--- frontend/admin/core/global/rebuild-required.tsx
+++ frontend/admin/core/global/rebuild-required.tsx
@@ -4,21 +4,30 @@ import { RefreshCcw } from "lucide-react";
 import { useTranslations } from "next-intl";
 
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 
 export const RebuildRequiredAdmin = () => {
   const t = useTranslations("admin.rebuild_required");
 
   return (
-    <Alert variant="destructive">
+    <Alert className="mb-2" variant="destructive">
       <RefreshCcw />
 
       <AlertTitle>{t("title")}</AlertTitle>
       <AlertDescription>{t("desc")}</AlertDescription>
-      <AlertDescription>{t("this_process")}</AlertDescription>
+      <AlertDescription>{t("why")}</AlertDescription>
 
       <div className="mt-2">
-        <Button variant="outline">{t("submit")}</Button>
+        <a
+          href="https://vitnode.com/help/rebuild"
+          rel="noopener noreferrer"
+          target="_blank"
+          className={buttonVariants({
+            variant: "outline"
+          })}
+        >
+          {t("submit")}
+        </a>
       </div>
     </Alert>
   );
