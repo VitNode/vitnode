89e37d1e1ec6b9ee6c652d9f8e8babe693ba0b9e
Andre <andriej2301@gmail.com>
2024-03-06 14:40:18 +0100

    fix: Authorization

--- backend/src/apps/core/sessions/devices/show/show.service.ts
+++ backend/src/apps/core/sessions/devices/show/show.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from "@nestjs/common";
-import { count } from "drizzle-orm";
+import { and, count, eq } from "drizzle-orm";
+
+import { DatabaseService } from "@/database/database.service";
+import { SortDirectionEnum } from "@/types/database/sortDirection.type";
+import { User } from "@/utils/decorators/user.decorator";
+import { inputPaginationCursor, outputPagination } from "@/functions/database/pagination";
+import { core_sessions } from "@/apps/admin/core/database/schema/sessions";
 
 import { ShowCoreSessionDevicesArgs } from "./dto/show.args";
 import { ShowCoreSessionDevicesObj } from "./dto/show.obj";
-
-import { DatabaseService } from "@/database/database.service";
-import { inputPaginationCursor, outputPagination } from "@/functions/database/pagination";
-
-import { SortDirectionEnum } from "@/types/database/sortDirection.type";
-import { core_sessions } from "@/apps/admin/core/database/schema/sessions";
 
 @Injectable()
 export class ShowCoreSessionDevicesService {
@@ -18,38 +18,42 @@ export class ShowCoreSessionDevicesService {
     cursor,
     first,
     last
-  }: ShowCoreSessionDevicesArgs): Promise<ShowCoreSessionDevicesObj> {
-    const pagination = await inputPaginationCursor({
-      cursor,
-      database: core_sessions,
-      databaseService: this.databaseService,
-      first,
-      last,
-      primaryCursor: { order: "ASC", key: "id", schema: core_sessions.user_id },
-      defaultSortBy: {
-        direction: SortDirectionEnum.asc,
-        column: "position"
-      }
-    });
+  }: ShowCoreSessionDevicesArgs,
+  user: User): 
+    Promise<ShowCoreSessionDevicesObj> {
+      const pagination = await inputPaginationCursor({
+        cursor,
+        database: core_sessions,
+        databaseService: this.databaseService,
+        first,
+        last,
+        primaryCursor: { order: "ASC", key: "id", schema: core_sessions.user_id },
+        defaultSortBy: {
+          direction: SortDirectionEnum.asc,
+          column: "position"
+        }
+      });
 
-    const edges = await this.databaseService.db.query.core_sessions.findMany({
-      ...pagination,
-      where: pagination.where,
-      with: {
-        device: true
-      }
-    });
+      const where = eq(core_sessions.user_id, user.id);
 
-    const totalCount = await this.databaseService.db
-      .select({ count: count() })
-      .from(core_sessions);    
+      const edges = await this.databaseService.db.query.core_sessions.findMany({
+        ...pagination,
+        where: and(pagination.where, where),
+        with: {
+          device: true
+        }
+      });
 
-    return outputPagination({
-      edges,
-      totalCount,
-      first,
-      cursor,
-      last
-    });
+      const totalCount = await this.databaseService.db
+        .select({ count: count() })
+        .from(core_sessions);    
+
+      return outputPagination({
+        edges,
+        totalCount,
+        first,
+        cursor,
+        last
+      });
   }
 }
