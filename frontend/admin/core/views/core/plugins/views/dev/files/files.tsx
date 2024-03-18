0227d84022e7b5bbd957cede193ab63709d1e5d6
aXenDeveloper <axendeveloper@gmail.com>
2024-03-18 18:52:15 +0100

    refactor(plugins): Improve files plugin page in AdminCP

--- frontend/admin/core/views/core/plugins/views/dev/files/files.tsx
+++ frontend/admin/core/views/core/plugins/views/dev/files/files.tsx
@@ -9,25 +9,26 @@ export const FilesDevPluginAdminView = ({
   const t = useTranslations("admin.core.plugins.download.check");
 
   return (
-    <ul>
+    <ul className="rounded-md border [&>li:not(:last-child)]:border-b max-w-[40rem]">
       {Object.entries(admin__core_plugins__files).map(item => (
-        <li key={item[0]}>
+        <li
+          className="p-4 flex gap-2 items-center justify-between"
+          key={item[0]}
+        >
           {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
           {/* @ts-expect-error */}
-          {t.rich(item[0], {
-            status: () => (
-              <span
-                className={cn("text-sm", {
-                  "text-muted-foreground italic": !item[1],
-                  "text-primary font-semibold": item[1],
-                  "text-destructive font-semibold":
-                    !item[1] && item[0] === "default_page"
-                })}
-              >
-                {t("file_detected", { count: +item[1] })}
-              </span>
-            )
-          })}
+          {t(item[0])}
+
+          <span
+            className={cn("text-sm text-right", {
+              "text-muted-foreground italic": !item[1],
+              "text-primary font-semibold": item[1],
+              "text-destructive font-semibold":
+                !item[1] && item[0] === "default_page"
+            })}
+          >
+            {t("file_detected", { count: +item[1] })}
+          </span>
         </li>
       ))}
     </ul>
