1acce8ec496f8fd5cd278a3586453681943b5dca
aXenDeveloper <axendeveloper@gmail.com>
2024-03-23 18:41:00 +0100

    feat(blog): Init files

--- backend/modules/modules.module.ts
+++ backend/modules/modules.module.ts
@@ -2,12 +2,14 @@ import { Module } from "@nestjs/common";
 
 import { ForumModule } from "./forum/forum.module";
 import { CoreModule } from "./core/core.module";
+import { BlogModule } from "./blog/blog.module";
 // ! === IMPORT ===
 
 @Module({
   imports: [
     CoreModule,
-    ForumModule
+    ForumModule,
+    BlogModule
     // ! === MODULE ===
   ]
 })
