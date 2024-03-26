85916829b98b0ceeb6df02624a09641637a355dc
aXenDeveloper <axendeveloper@gmail.com>
2024-03-26 10:57:51 +0100

    feat(blog): Add resolver for show categories

--- backend/plugins/blog/categories/categories.module.ts
+++ backend/plugins/blog/categories/categories.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 
 import { ShowBlogCategoriesService } from "./show/show.service";
+import { ShowBlogCategoriesResolver } from "./show/show.resolver";
 
 @Module({
-  providers: [ShowBlogCategoriesService]
+  providers: [ShowBlogCategoriesService, ShowBlogCategoriesResolver]
 })
 export class BlogCategoriesModule {}
