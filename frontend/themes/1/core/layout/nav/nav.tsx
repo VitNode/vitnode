4ea0bc9fc7a28bbf76fd6209a2de0e2d3a840dea
aXenDeveloper <axendeveloper@gmail.com>
2024-04-01 09:37:36 +0200

    perf(frontend): Improve colors

--- frontend/themes/1/core/layout/nav/nav.tsx
+++ frontend/themes/1/core/layout/nav/nav.tsx
@@ -22,7 +22,7 @@ export const Nav = () => {
         className="absolute left-0 top-full flex justify-center"
         style={{ perspective: "2000px" }}
       >
-        <NavigationMenu.Viewport className="origin-top-center relative mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded-md border bg-popover text-popover-foreground shadow data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[var(--radix-navigation-menu-viewport-width)]" />
+        <NavigationMenu.Viewport className="origin-top-center relative mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded-md border bg-popover text-popover-foreground shadow data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[var(--radix-navigation-menu-viewport-width)] transition-all" />
       </div>
     </NavigationMenu.Root>
   );
