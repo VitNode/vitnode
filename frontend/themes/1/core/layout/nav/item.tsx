873247461b5719057a0006e198d29b8b39217f81
aXenDeveloper <axendeveloper@gmail.com>
2024-04-02 16:11:06 +0200

    chore(nav): Improve columns

--- frontend/themes/1/core/layout/nav/item.tsx
+++ frontend/themes/1/core/layout/nav/item.tsx
@@ -7,11 +7,6 @@ import { useTextLang } from "@/hooks/core/use-text-lang";
 import { Link, usePathname } from "@/i18n";
 import { cn } from "@/functions/classnames";
 import { Icon } from "@/components/icon/icon";
-import {
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink
-} from "@/components/ui/navigation-menu";
 
 export const ItemNav = ({
   children,
@@ -26,16 +21,18 @@ export const ItemNav = ({
     href === pathname || (pathname.startsWith(href) && href !== "/");
 
   return (
-    <NavigationMenuItem className="flex-shrink-0">
+    <NavigationMenu.Item className="flex-shrink-0">
       <NavigationMenu.Trigger asChild>
         <Link
           href={href}
-          className={buttonVariants({
-            variant: "ghost",
-            className: cn("px-6", {
-              "bg-accent": active
+          className={cn(
+            buttonVariants({
+              variant: active ? "outline" : "ghost",
+              className: cn("px-6", {
+                "bg-card dark:bg-accent": active
+              })
             })
-          })}
+          )}
           target={external ? "_blank" : undefined}
           rel={external ? "noopener noreferrer" : undefined}
         >
@@ -45,16 +42,30 @@ export const ItemNav = ({
       </NavigationMenu.Trigger>
 
       {children.length > 0 && (
-        <NavigationMenuContent>
-          <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+        <NavigationMenu.Content
+          className={cn(
+            "top-0 left-0 duration-200 ease-in-out data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 w-56",
+            {
+              "w-[30rem]": children.length >= 3,
+              "lg:w-[50rem]": children.length >= 5
+            }
+          )}
+        >
+          <ul className="flex gap-1 flex-wrap p-2">
             {children.map(item => {
               const activeItem =
                 item.href === pathname ||
                 (pathname.startsWith(item.href) && item.href !== "/");
 
               return (
-                <li key={item.id}>
-                  <NavigationMenuLink asChild>
+                <li
+                  key={item.id}
+                  className={cn("flex-1 basis-full", {
+                    "basis-[calc(50%-0.5rem)]": children.length >= 3,
+                    "lg:basis-[calc(33%-0.5rem)]": children.length >= 5
+                  })}
+                >
+                  <NavigationMenu.Link asChild>
                     <Link
                       href={item.href}
                       className={cn(
@@ -78,13 +89,13 @@ export const ItemNav = ({
                         </p>
                       )}
                     </Link>
-                  </NavigationMenuLink>
+                  </NavigationMenu.Link>
                 </li>
               );
             })}
           </ul>
-        </NavigationMenuContent>
+        </NavigationMenu.Content>
       )}
-    </NavigationMenuItem>
+    </NavigationMenu.Item>
   );
 };
