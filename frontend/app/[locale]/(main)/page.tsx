9ef19747bea7e220e4251c920b361a56d3a96044
aXenDeveloper <axendeveloper@gmail.com>
2024-04-15 17:44:46 +0200

    feat(frontend): Add copyright to footer

--- frontend/app/[locale]/(main)/page.tsx
+++ frontend/app/[locale]/(main)/page.tsx
@@ -3,9 +3,37 @@ import type { Metadata } from "next";
 
 import { getSessionData } from "@/functions/get-session-data";
 
-export async function generateMetadata(): Promise<Metadata> {
+interface Props {
+  params: {
+    locale: string;
+  };
+}
+
+const getDescription = async ({
+  locale
+}: {
+  locale: Props["params"]["locale"];
+}): Promise<string> => {
+  const {
+    data: {
+      core_settings__show: { site_description }
+    }
+  } = await getSessionData();
+
+  const textFromLang = site_description.find(t => t.language_code === locale);
+
+  if (textFromLang) {
+    return textFromLang.value;
+  }
+
+  return site_description[0].value;
+};
+
+export async function generateMetadata({
+  params: { locale }
+}: Props): Promise<Metadata> {
   return {
-    description: "Page description"
+    description: await getDescription({ locale })
   };
 }
 
