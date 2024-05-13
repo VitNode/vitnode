84cdc112651d2c11d92fc92713d35b0fb30d5c4f
aXenDeveloper <axendeveloper@gmail.com>
2024-05-13 14:49:16 +0200

    chore: Clean up in package

--- docker-prod.sh
+++ docker-prod.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Stop and remove all containers
-# docker rm vitnode_database --force
-# docker rm vitnode_backend --force
-# docker rm vitnode_frontend --force
-
 # Run docker-compose
 docker-compose -f ./docker-compose.yml -p vitnode up -d --build
 
