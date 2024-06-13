dc44990332d3c3be1033804836d30ec7974d72b7
Andre <andriej2301@gmail.com>
2024-06-13 08:37:16 +0200

    fix: Fixed typing in resolver

--- backend/src/plugins/core/admin/members/edit/edit.resolver.ts
+++ backend/src/plugins/core/admin/members/edit/edit.resolver.ts
@@ -5,6 +5,7 @@ import { EditAdminMembersService } from "./edit.service";
 import { EditAdminMembersArgs } from "./dto/edit.args";
 
 import { AdminAuthGuards } from "@/utils/guards/admin-auth.guard";
+import { EditAdminMembersObj } from "./dto/edit.obj";
 
 @Resolver()
 export class EditAdminMembersResolver {
@@ -14,7 +15,7 @@ export class EditAdminMembersResolver {
   @UseGuards(AdminAuthGuards)
   async admin__core_members__edit(
     @Args() args: EditAdminMembersArgs
-  ): Promise<string> {
+  ): Promise<EditAdminMembersObj> {
     return this.service.edit(args);
   }
 }
