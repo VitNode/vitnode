3379f1062a3025061433c3c7b02330a295f474ed
aXenDeveloper <axendeveloper@gmail.com>
2024-06-19 22:40:35 +0200

    perf(backend): Move config to package

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,27 +1,21 @@
 import { join } from "path";
 
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
 import { VitNodeCoreModule } from "vitnode-backend";
 
 import { DatabaseModule } from "./database/database.module";
 import { PluginsModule } from "./plugins/plugins.module";
-import { ABSOLUTE_PATHS, configForAppModule } from "./config";
+import { schemaDatabase } from "./database/schema";
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [configForAppModule],
-      envFilePath: join(process.cwd(), "..", "..", ".env")
-    }),
     PluginsModule,
     DatabaseModule,
     VitNodeCoreModule.register({
       paths: {
-        uploadPublic: ABSOLUTE_PATHS.uploads.public,
-        graphqlSchema: join(process.cwd(), "schema.gql")
-      }
+        envFile: join(process.cwd(), "..", "..", ".env")
+      },
+      schemaDatabase
     })
   ]
 })
