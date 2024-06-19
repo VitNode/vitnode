3cebb3ef01a480dbf9337ea946f31d87dfffbe4f
aXenDeveloper <axendeveloper@gmail.com>
2024-06-19 17:31:23 +0200

    feat(backend): Add nestjsMainApp function

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,31 +1,22 @@
 /* eslint-disable no-console */
 import { NestFactory } from "@nestjs/core";
-import cookieParser from "cookie-parser";
-import { ValidationPipe } from "@nestjs/common";
-import { graphqlUploadExpress } from "vitnode-backend";
+import { INestApplication } from "@nestjs/common";
+import { nestjsMainApp } from "vitnode-backend";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
-  app.enableCors({
-    credentials: true,
-    origin: [
-      process.env.NEXT_PUBLIC_FRONTEND_URL
-        ? process.env.NEXT_PUBLIC_FRONTEND_URL
-        : "http://localhost:3000",
-      "https://sandbox.embed.apollographql.com"
-    ]
-  });
-  app.use(graphqlUploadExpress({ maxFiles: 100 }));
+  const app: INestApplication = await NestFactory.create(AppModule);
 
-  // Class Validation
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true
-    })
-  );
+  nestjsMainApp(app, {
+    cors: {
+      origin: [
+        process.env.NEXT_PUBLIC_FRONTEND_URL
+          ? process.env.NEXT_PUBLIC_FRONTEND_URL
+          : "http://localhost:3000"
+      ]
+    }
+  });
 
   await app.listen(process.env.PORT ?? "8080", null, () => {
     console.log(
