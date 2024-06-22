424b38109ec3960f969c1ea35f1dcd941a5e5484
aXenDeveloper <axendeveloper@gmail.com>
2024-06-22 11:32:23 +0200

    perf(backend): Move email module to package

--- apps/backend/src/plugins/core/theme_editor/theme_editor.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-
-import { ShowCoreThemeEditorResolver } from "./show/show.resolver";
-import { ShowCoreThemeEditorService } from "./show/show.service";
-
-@Module({
-  providers: [ShowCoreThemeEditorResolver, ShowCoreThemeEditorService]
-})
-export class CoreThemeEditorModule {}
---
+++ packages/backend/src/core/admin/email/settings/show/show.resolver.ts
@@ -0,0 +1,17 @@
+import { Query, Resolver } from "@nestjs/graphql";
+import { UseGuards } from "@nestjs/common";
+
+import { ShowAdminEmailSettingsServiceObj } from "./dto/show.obj";
+import { ShowAdminEmailSettingsService } from "./show.service";
+import { AdminAuthGuards } from "../../../../../utils";
+
+@Resolver()
+export class ShowAdminEmailSettingsResolver {
+  constructor(private readonly service: ShowAdminEmailSettingsService) {}
+
+  @Query(() => ShowAdminEmailSettingsServiceObj)
+  @UseGuards(AdminAuthGuards)
+  admin__core_email_settings__show(): ShowAdminEmailSettingsServiceObj {
+    return this.service.show();
+  }
+}
