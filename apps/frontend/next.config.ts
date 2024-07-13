3fc1c6f0602bc1cfd86861c0cb1987ecfd7113a3
aXenDeveloper <axendeveloper@gmail.com>
2024-07-13 09:15:20 +0200

    perf(frontend): Move css admin file to package

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,16 +1,6 @@
 import { NextConfig } from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
+import VitNodeConfig from 'vitnode-frontend/next.config';
 
-const withNextIntl = createNextIntlPlugin();
+const nextConfig: NextConfig = {};
 
-const nextConfig: NextConfig = {
-  // TODO: Remove this when the framer-motion issue is fixed for React 19
-  reactStrictMode: false,
-  experimental: {
-    ppr: true,
-  },
-  output: 'standalone',
-  transpilePackages: ['lucide-react', 'vitnode-shared', 'vitnode-frontend'],
-};
-
-export default withNextIntl(nextConfig);
+export default VitNodeConfig(nextConfig);
