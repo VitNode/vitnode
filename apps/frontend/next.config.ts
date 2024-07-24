ee7c30d4c6e91655e2a7c45424d33e9eab360af8
aXenDeveloper <axendeveloper@gmail.com>
2024-07-24 12:32:24 +0200

    chore: Add bundle-analyzer

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,6 +1,11 @@
 import { NextConfig } from 'next';
 import VitNodeConfig from 'vitnode-frontend/next.config';
+import bundleAnalyzer from '@next/bundle-analyzer';
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 const nextConfig: NextConfig = {};
 
-export default VitNodeConfig(nextConfig);
+export default withBundleAnalyzer(VitNodeConfig(nextConfig));
