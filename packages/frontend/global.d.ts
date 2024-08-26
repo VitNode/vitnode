628722603121b8359a706bf1bb2305dc9a92f19c
aXenDeveloper <axendeveloper@gmail.com>
2024-08-26 13:12:49 +0200

    chore: Fix dir for en.json in package frontend

--- packages/frontend/global.d.ts
+++ packages/frontend/global.d.ts
@@ -1,5 +1,5 @@
-import type core from '../../apps/frontend/plugins/core/langs/en.json';
-import type admin from '../../apps/frontend/plugins/admin/langs/en.json';
+import type core from '../../apps/frontend/src/plugins/core/langs/en.json';
+import type admin from '../../apps/frontend/src/plugins/admin/langs/en.json';
 
 type Messages = typeof core & typeof admin;
 
