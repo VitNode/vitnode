e567c2b06822d8042c81f58ec96fc3cd11889745
aXenDeveloper <axendeveloper@gmail.com>
2024-08-31 12:57:17 +0200

    perf: Delete update lang in AdminCP

--- packages/backend/src/core/admin/languages/languages.module.ts
+++ packages/backend/src/core/admin/languages/languages.module.ts
@@ -8,8 +8,6 @@ import { DownloadAdminCoreLanguagesResolver } from './download/download.resolver
 import { DownloadAdminCoreLanguageService } from './download/download.service';
 import { EditAdminCoreLanguagesResolver } from './edit/edit.resolver';
 import { EditAdminCoreLanguagesService } from './edit/edit.service';
-import { UpdateAdminCoreLanguagesResolver } from './update/update.resolver';
-import { UpdateAdminCoreLanguageService } from './update/update.service';
 
 @Module({
   providers: [
@@ -21,8 +19,6 @@ import { UpdateAdminCoreLanguageService } from './update/update.service';
     DeleteAdminCoreLanguageService,
     DownloadAdminCoreLanguageService,
     DownloadAdminCoreLanguagesResolver,
-    UpdateAdminCoreLanguageService,
-    UpdateAdminCoreLanguagesResolver,
   ],
 })
 export class AdminLanguagesModule {}
