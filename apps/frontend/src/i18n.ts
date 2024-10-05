d6e2de12dbcde88d8cad57139b146a251442b7e5
aXenDeveloper <axendeveloper@gmail.com>
2024-10-05 11:42:21 +0200

    chore: Update eslint

--- apps/frontend/src/i18n.ts
+++ apps/frontend/src/i18n.ts
@@ -2,10 +2,12 @@ import { getRequestConfig } from 'next-intl/server';
 import { i18nConfigVitNode } from 'vitnode-frontend/i18n';
 
 export default getRequestConfig(async ({ requestLocale }) => {
-  return await i18nConfigVitNode({
+  const config = await i18nConfigVitNode({
     pathsToMessagesFromPlugins: async ({ plugin, locale }) => {
       return await import(`./plugins/${plugin}/langs/${locale}.json`);
     },
     requestLocale,
   });
+
+  return config;
 });
