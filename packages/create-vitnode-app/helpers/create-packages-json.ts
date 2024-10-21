113da9d087da5345026be38991a65ca665ba9c99
aXenDeveloper <axendeveloper@gmail.com>
2024-10-21 18:53:57 +0200

    fix: Create/delete plugins while restarting server

--- packages/create-vitnode-app/helpers/create-packages-json.ts
+++ packages/create-vitnode-app/helpers/create-packages-json.ts
@@ -99,7 +99,7 @@ export const createPackagesJSON = ({
       geist: '^1.3.1',
       'lucide-react': '^0.451.0',
       next: '15.0.0-rc.1',
-      'next-intl': '^3.21.0-canary.0',
+      'next-intl': '3.21.0-canary.0',
       react: '19.0.0-rc-6cf85185-20241014',
       'react-dom': '19.0.0-rc-6cf85185-20241014',
       'react-hook-form': '^7.53.0',
